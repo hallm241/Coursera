87a33cc1fbd7cd3dec674e216706573cc7d942d9
Matthew <hallm241@users.noreply.github.com>
2016-07-09 19:08:08 +1000

    Added dplyr package and complete 2 swirl assignments on it (added some scripts to help with understanding of summarise and pipe)

--- Data_clean/Data_Clean_Packages.R
+++ Data_clean/Data_Clean_Packages.R
@@ -3,6 +3,7 @@ install.packages("sqldf")       # for reading SQL into R data frames
 install.packages("httr")        # GET/POST commands etc.
 install.packages("httpuv")      # required in combo with httr to help authenticate github
 install.packages("jsonlite")    # reading and converting json files
+install.packages("dplyr")       # helps you work with dataframes
 
 source("http://bioconductor.org/biocLite.R") # for getting the HDF5 package
 biocLite("rhdf5")
