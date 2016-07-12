59eeec2c7f5bd5875f61320bd017838164e4576c
Matthew <hallm241@users.noreply.github.com>
2016-07-12 23:16:33 +1000

    Completed quiz 2 for exploratory data analysis

---
+++ Exploratory_Data_Analysis/Quiz2.R
@@ -0,0 +1,41 @@
+#q2
+library(nlme)
+library(lattice)
+xyplot(weight ~ Time | Diet, BodyWeight)
+
+# note - BodyWeight is dataset... hiden in nlme or lattice package?
+
+#q4
+library(lattice)
+library(datasets)
+data(airquality)
+p <- xyplot(Ozone ~ Wind | factor(Month), data = airquality)
+
+#q5
+?trellis.par.set
+
+#q7
+library(ggplot2)
+library(datasets)
+data(airquality)
+
+qplot(Wind, Ozone, data = airquality)
+
+#this one
+airquality = transform(airquality, Month = factor(Month))
+qplot(Wind, Ozone, data = airquality, facets = . ~ Month)
+
+qplot(Wind, Ozone, data = airquality, facets = . ~ factor(Month))
+
+qplot(Wind, Ozone, data = airquality, geom = "smooth")
+
+#q9
+library(ggplot2)
+library(ggplot2movies)
+g <- ggplot(movies, aes(votes, rating))
+print(g)
+
+#q10
+library(ggplot2)
+qplot(votes, rating, data = movies)
+
